588278236324666c9cf8d00799a4be3095f1396f
amar-nath1 <amarat279955>
2022-11-17 16:27:34 +0530

    adding Expense Items React

---
+++ App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./Components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h1>Expense Items</h1>
+    <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Components/ExpenseItem.js
@@ -0,0 +1,15 @@
+
+function ExpenseItem(){
+
+    return(
+        <>
+        <h2>Food Rs. 10</h2>
+
+        <h2>Petrol Rs. 100</h2>
+    
+        <h2>Movies Rs. 200</h2>
+        </>
+    )
+}
+
+export default ExpenseItem
