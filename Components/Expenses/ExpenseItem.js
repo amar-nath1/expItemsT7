18560e0065bc67f78d3bcee7b05cd1686a7226a9
amar-nath1 <amarat279955>
2022-11-19 15:28:17 +0530

    amount will be update to 1000 on click

--- Components/Expenses/ExpenseItem.js
+++ Components/Expenses/ExpenseItem.js
@@ -2,11 +2,15 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
 import Card from '../UI/Card'
+import React, { useState } from 'react'
 
 
 const ExpenseItem=(props) =>{
-function clickHandler(e){
-e.target.parentElement.remove()
+const [title,setTitle]=useState(props.title)
+const [amt,setAmt]=useState(props.amount)
+function clickHandler(){
+setTitle('updated')
+setAmt(1000)
     
 }
 
@@ -15,7 +19,7 @@ e.target.parentElement.remove()
         <Card className="expense-item">
 
             <ExpenseDate date={props.date}></ExpenseDate>
-            <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails>
+            <ExpenseDetails title={title} amount={amt} location={props.location}></ExpenseDetails>
             <button onClick={clickHandler}>Delete Expense</button>
             
         </Card>
