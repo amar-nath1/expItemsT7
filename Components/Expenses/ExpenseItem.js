4c665b150209383fbe80b7d71a381f9de5ef43f3
amar-nath1 <amarat279955>
2022-11-22 12:25:48 +0530

    added filter functionality

--- Components/Expenses/ExpenseItem.js
+++ Components/Expenses/ExpenseItem.js
@@ -2,25 +2,19 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
 import Card from '../UI/Card'
-import React, { useState } from 'react'
+
 
 
 const ExpenseItem=(props) =>{
-const [title,setTitle]=useState(props.title)
-const [amt,setAmt]=useState(props.amount)
-function clickHandler(){
-setTitle('updated')
-setAmt(1000)
-    
-}
+
+
 
     return (
         <>
         <Card className="expense-item">
 
             <ExpenseDate date={props.date}></ExpenseDate>
-            <ExpenseDetails title={title} amount={amt} location={props.location}></ExpenseDetails>
-            <button onClick={clickHandler}>Delete Expense</button>
+            <ExpenseDetails title={props.title} amount={props.amount}></ExpenseDetails> 
             
         </Card>
 
--- Components/Expenses/Expenses.js
+++ Components/Expenses/Expenses.js
@@ -8,7 +8,7 @@ import React,{ useState } from "react";
 import Card from "../UI/Card";
 const Expenses = (props) => {
 
-  const [filteredYear,setFilterYear]=useState('2020')
+  const [filteredYear,setFilterYear]=useState('All')
   
 
 const filterChangeHandler=(selectedYear)=>{
@@ -17,14 +17,22 @@ const filterChangeHandler=(selectedYear)=>{
   
 }
 
+let filteredItems=props.items.filter((exp)=>{
 
+  return exp.date.getFullYear().toString()===filteredYear
+})
 
+let arrToShow
 
+if (filteredYear==='All'){
+  arrToShow=props.items
+}
+else{arrToShow=filteredItems}
 
   return (
     <Card className='expenses'>
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpenseFilter>
-      {props.items.map((exp)=>(
+      {arrToShow.map((exp)=>(
 
         <ExpenseItem key={exp.key} title={exp.title} amount={exp.amount} date={exp.date}></ExpenseItem>
 
